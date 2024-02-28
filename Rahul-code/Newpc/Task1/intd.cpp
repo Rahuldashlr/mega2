e521f3a4ac1bb88a750c7617b6ad9db787d1baec
Rahuldashlr <147247920+Rahuldashlr@users.noreply.github.com>
2024-02-28 13:27:57 +0530

    a

--- Rahul-code/Newpc/Task1/intd.cpp
+++ Rahul-code/Newpc/Task1/intd.cpp
@@ -6,6 +6,7 @@ int main() { // we write int main ();
 
 
 int n; //  n is a input as a integer from user. which will hold the input value from the user
+cout << " please enter a Numaric Veriable " << endl;
 cin>>n; // The program then uses the cin >> n syntax to wait for the user to enter a number.
 
 int i=1; // The program then initializes an integer variable i to 1.
@@ -14,7 +15,7 @@ while(i<=n) {  //The program then uses a while loop to iterate until the conditi
     
                       
   cout<<i<<""; //Inside the while loop, the program prints the value of i to the console and increments i by 1.
-    i=i+1;           
+    i=i+1  ;        
 
  
 
