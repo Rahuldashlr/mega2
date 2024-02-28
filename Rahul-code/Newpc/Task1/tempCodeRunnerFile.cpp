5c25a684c80ef4e587941c0fd1dbf74e0610d745
Rahuldashlr <147247920+Rahuldashlr@users.noreply.github.com>
2024-02-28 13:22:21 +0530

    z

---
+++ Rahul-code/Newpc/Task1/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+
+
+int i=1; // The program then initializes an integer variable i to 1.
+
+while(i<=n) {  //The program then uses a while loop to iterate until the condition i <= n is no longer true. 
+    
+                      
