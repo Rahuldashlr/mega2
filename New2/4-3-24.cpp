416fbf97e519863c0e0347c3b02132d5d13e4637
Rahuldashlr <147247920+Rahuldashlr@users.noreply.github.com>
2024-03-04 21:27:50 +0530

    w

---
+++ New2/4-3-24.cpp
@@ -0,0 +1,29 @@
+#include <iostream> //starting argument
+using namespace  std; // which predefined the namespace using std; or we need to do  for an print as :- 
+                      //cout std::cout << " hello word " <<endl; 
+
+
+int main()   //main intput 
+
+      {
+       
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+       }
+       
