3d5d8018b2b3fa144390c1b35038a9653a4a9661
Rahuldashlr <147247920+Rahuldashlr@users.noreply.github.com>
2024-03-05 22:36:58 +0530

    a

--- New2/4-3-24.cpp
+++ New2/4-3-24.cpp
@@ -1,29 +1,31 @@
-#include <iostream> //starting argument
-using namespace  std; // which predefined the namespace using std; or we need to do  for an print as :- 
-                      //cout std::cout << " hello word " <<endl; 
+#include <iostream>
+using namespace std;
 
+int main() {
 
-int main()   //main intput 
+  int i, n;
+  bool is_prime = true;
 
-      {
-       
-        
+  cout << "Enter a positive integer: ";
+  cin >> n;
 
+  // 0 and 1 are not prime numbers
+  if (n == 0 || n == 1) {
+    is_prime = false;
+  }
 
+  // loop to check if n is prime
+  for (i = 2; i <= n/2; ++i) {
+    if (n % i == 0) {
+      is_prime = false;
+      break;
+    }
+  }
 
+  if (is_prime)
+    cout << n << " is a prime number";
+  else
+    cout << n << " is not a prime number";
 
-
-
-
-
-
-
-
-
-
-
-
-
-       
-       }
-       
+  return 0;
+}
